27f509d2c9e822368176be7bea190900d29ee239
jorge <jorge.ignacio131198@gmail.com>
2022-04-27 02:46:36 -0400

    iniciando sistema de roles

--- codigos/entrar3.php
+++ codigos/entrar3.php
@@ -11,6 +11,8 @@ session_start(); //Paso 1 para utilizar sesiones
  
     $validar_login = $mysqli->query("SELECT * FROM datos_usuarios WHERE EMAIL LIKE '{$email}'
      and  QPASSWORD = '{$password}' ");
+
+     
     if(empty($validar_login->num_rows)){ 
         array_push($error, "Las credenciales no coinciden ");
         echo'
@@ -22,8 +24,31 @@ session_start(); //Paso 1 para utilizar sesiones
         
         }else{ //si coinciden
         $_SESSION['usuario'] = $email;      //variable de sesion
+
+
+        $query = "SELECT * FROM datos_usuarios  WHERE EMAIL LIKE '{$email}' and  QPASSWORD = '{$password}'";
+        $sentencia1 = $mysqli->query($query);
+        //print_r($sentencia1);  no entrega nada importante la sentencia es importante para la segunda.
+        $sentencia2 =mysqli_fetch_array($sentencia1);
+        //print_r($sentencia2['NIVEL']);    nivel
         
-        header("location: ../vistas/welcome.php");
+        if($sentencia2['NIVEL'] == 1){
+            header("location: ../vistas/usuarios.php");
+        }
+
+        if($sentencia2['NIVEL'] == 2){
+            header("location: ../vistas/welcome.php");
+        }
+        
+        if($sentencia2['NIVEL'] == 3){
+            header("location: ../vistas/welcome.php");
+        }
+        
+        
+
+
+        
+
         }
         exit;
 
