263848416ef4c21e443e7e28002d4257a082f513
jorge <jorge.ignacio131198@gmail.com>
2022-05-23 21:18:20 -0400

    galeria lista 100% xd

--- tienda/admin/productos/d_galeria.php
+++ tienda/admin/productos/d_galeria.php
@@ -19,10 +19,11 @@ $id_foto = $_GET['id_foto'];
 
 
 
+$query1 = "SELECT * FROM galeria WHERE ID_G = $id_foto";
+$sentencia1 = mysqli_query($mysqli, $query1);
+$sentencia2 = mysqli_fetch_array($sentencia1);
 
-
-
-
+unlink("img/$sentencia2[FOTO]");
 
 
 
@@ -30,11 +31,13 @@ $id_foto = $_GET['id_foto'];
     
 
     //Eliminando los datos
-$query = "DELETE FROM galeria WHERE ID_G = $id_foto ";
+$query2 = "DELETE FROM galeria WHERE ID_G = $id_foto ";
 
 
 
-if(mysqli_query($mysqli, $query)){
+
+if(mysqli_query($mysqli, $query2)){
+    //Eliminando imagen del servidor
     header("Location: E_producto.php?mensaje=eliminado&codigo=$codigo"); //Enviandole 2 POST, EL CODIGO Y LA ALERTA. 2 GETS.
     
 }else{
