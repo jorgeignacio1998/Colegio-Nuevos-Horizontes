ea5b98e45907a65386a4d8cc50ce7c14a36c3db8
jorge <jorge.ignacio131198@gmail.com>
2022-06-06 03:52:20 -0400

    slider subido falta ponerlo

--- tienda/index.php
+++ tienda/index.php
@@ -4,47 +4,18 @@
 ?>
 
 
+
+<body>
+
+
+ 
     <br><br>
 
 
-
-
-
     
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<br><br><br><br>
-
-
-
-
-
-
-
-
-
-    
+    <br><br><br><br>
     <main class="main">
             <div class="container">
                 <h2 class="main-title">Nuevos productos</h2>
@@ -79,75 +50,6 @@
 
             </section>
 
-
-
-
-
-
-
-
-<!-- 
-                <div class="product">
-                    <img src="img/prod2/b2.PNG" alt="" class="product__img">
-                    <div class="product__description">
-                        <h3 class="product__title">Just one Sec 2020</h3>
-                        <span class="product__price">$49.990</span>
-                    </div>
-                    <i class="product__icon fa-solid fa-cart-plus"></i>
-                </div>
-                <div class="product">
-                    <img src="img/prod2/b3.PNG" alt="" class="product__img">
-                    <div class="product__description">
-                        <h3 class="product__title">Dirty Light s900</h3>
-                        <span class="product__price">$35.990</span>
-                    </div>
-                    <i class="product__icon fa-solid fa-cart-plus"></i>
-                </div>
-                <div class="product">
-                    <img src="img/prod2/b4.PNG" alt="" class="product__img">
-                    <div class="product__description">
-                        <h3 class="product__title">Sunglasses AK-47 </h3>
-                        <span class="product__price">$45.990</span>
-                    </div>
-                    <i class="product__icon fa-solid fa-cart-plus"></i>
-                </div>
-
-
-
-
-
-                <div class="product">
-                    <img src="img/prod2/b5.PNG" alt="" class="product__img">
-                    <div class="product__description">
-                        <h3 class="product__title">Forever alone Forza Edition</h3>
-                        <span class="product__price">$33.990</span>
-                    </div>
-                    <i class="product__icon fa-solid fa-cart-plus"></i>
-                </div>
-                <div class="product">
-                    <img src="img/prod2/b6.PNG" alt="" class="product__img">
-                    <div class="product__description">
-                        <h3 class="product__title">Can't Dodge Dude!</h3>
-                        <span class="product__price">$29.990</span>
-                    </div>
-                    <i class="product__icon fa-solid fa-cart-plus"></i>
-                </div>
-                <div class="product">
-                    <img src="img/prod2/b7.PNG" alt="" class="product__img">
-                    <div class="product__description">
-                        <h3 class="product__title">Big Time Valentine</h3>
-                        <span class="product__price">$35.990</span>
-                    </div>
-                    <i class="product__icon fa-solid fa-cart-plus"></i>
-                </div>
-                <div class="product">
-                    <img src="img/prod2/b8.PNG" alt="" class="product__img">
-                    <div class="product__description">
-                        <h3 class="product__title">Never Surrender VIP</h3>
-                        <span class="product__price">$79.990</span>
-                    </div>
-                    <i class="product__icon fa-solid fa-cart-plus"></i>
-                </div> -->
 
            
     </main>
---
+++ tienda/slider_2/index.php
@@ -0,0 +1,98 @@
+<?php  
+    include '../codes/connect.php';
+  
+
+
+
+    $querySlider = ("SELECT * FROM slider_imagenes ");
+    $resultadoSlider = mysqli_query($mysqli, $querySlider);
+    $variable = 1;
+    foreach( $resultadoSlider as $row ){ 
+        $imagenes[]= "img/$row[NOMBRE_IMAGEN]"; 
+        $imagenes2[]= "'img/$row[NOMBRE_IMAGEN]'"; 
+        $variable++;
+    } 
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>  <!-- Ajax cdn jquery 3.6 -->
+</head>
+
+<style>
+    
+.contene{
+    position: relative;
+    z-index: 999;
+    height: 630px;
+    
+}
+
+.img{
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top:0;
+    left:0;
+    object-fit: cover;
+}
+
+
+</style>
+
+
+
+<body>
+    <header class="main-header">
+
+        <div class="contene">
+            <img id='slideshow2'  class="img"                  src='<?php echo $imagenes[0]; ?>'  /> 
+        </div>
+        <br style='clear:both;'>
+        <br style='clear:both;'>
+    
+        <br><br><br><br>
+    
+        </section>
+    </header>
+
+
+    <script>
+        const imagenes = [<?php echo implode(',', $imagenes2); ?>]
+    $(document).ready( function(){
+        $( imagenes ).each(function( index ) { 
+            setTimeout(() => {  
+                $('#slideshow2').attr('src', imagenes[index])
+            }, 4000 * index)
+        });
+    });
+
+</script>
+
+  
+</body>
+
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+</html>
