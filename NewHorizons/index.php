276737be85f4100b0d9610ba077257fddc8498b2
jorge <jorge.ignacio131198@gmail.com>
2022-06-17 01:51:03 -0400

    index decente

--- NewHorizons/index.php
+++ NewHorizons/index.php
@@ -21,6 +21,7 @@ if(isset($_SESSION['usuario'])){  //en el caso que exista alguien logueado va a 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="slider_1/css/estilos.css?<?php echo time(); ?>" >  <!-- CSS  del SLIDER-->
+    <link rel="stylesheet" type="text/css" href="navbar_1/styles.css?<?php echo time(); ?>" >  <!-- CSS  del navbar-->
     <title>Nuevos Horizontes</title>
     
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -34,9 +35,7 @@ if(isset($_SESSION['usuario'])){  //en el caso que exista alguien logueado va a 
    height: 6rem;
    width: 6rem;
 }
-.largo{
-   height: 6rem;
-}
+
 
 
 </style>
@@ -46,53 +45,88 @@ if(isset($_SESSION['usuario'])){  //en el caso que exista alguien logueado va a 
 
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-   <!-- Inicio del Navbar -->
-      <nav class="navbar  navbar-expand-md border-primary navbar-dark bg-primary largo ">
-         <div class="container-fluid">
 
-               <a href="index.php" class="navbar-brand"><img src="img/logo.png" class="logo"></a>
-               <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#MenuNav" >
-                  <span class="navbar-toggler-icon"></span>
-               </button>
-               
-               <div id="MenuNav" class="collapse navbar-collapse d-flex flex-row-reverse">
-                  <ul class="navbar-nav" ms-3>
-                     <!--  <li class="nav-item"> <a class="nav-link" href="perfil.php">Perfíl</a></li> -->
-                     
-                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
-                           Opciones de soporte
-                        </a>
-                        <ul class="dropdown-menu">
-                        <li> <a class="dropdown-item" href="NewHorizons/">Opcion 1</a></li>
-                        <li> <a class="dropdown-item" href="NewHorizons/">Opcion 2</a></li>
-                        <li> <a class="dropdown-item" href="NewHorizons/">Opcion 3</a></li>
-                        <?php if($contador == 0){
-                           echo  '<li> <a class="dropdown-item" href="views/login.php">Iniciar sesión</a></li>';
-                        }else{
-                           echo   '<li> <a class="dropdown-item" href="codes/logout.php">Cerrar sesión</a></li>';
-                        }?>
-                        
-                        
-                        </ul>
-                     </li>
-                  </ul>
-               </div>
+
+
+
+
+
+
+
+   <!-- Navbar  -->
+      <header>
+         <div class="logo-header" >
+            <img src="img/logo.png"  >
          </div>
-      </nav>
-   <!-- Termino del Navbar  -->
+         <nav class="nav-menu">
+            <ul>
+               <li class="ingresar"><a href="views/login.php">INGRESAR</a></li>
+            </ul>
+         </nav>
+      </header>
+   <!-- Navbar  -->
 
+   
 
-
-   <!-- HTML SLIDER  -->
+   <!--  SLIDER  -->
       <?php  
          include 'slider_1/index.php';
       ?>
-   <!-- HTML SLIDER  -->
+   <!--  SLIDER  -->
 
 
+   <!--  CUERPO  -->
+      <br> <br> <br> 
+      <div class="grid-layout-prin">
+         <h2>¡Gracias por ser parte de Colegio Nuevos Horizontes!</h2>
+      </div> <br>
 
-  
+      <div class="row d-flex justify-content-center">
+         <div class="caja col-sm-3">
+               <h2>ADMISIÓN 2023</h2>
+               <p> En el colegio Nuevos Horizontes estamos preparados para recibir a nuevos estudiantes en la comunidad educativa. Puedes encontrar más información en el sitio web del colegio para saber como continuar con las matriculas de alumnos ya ingresados. </p>
+         </div>
+
+         <div class="caja col-sm-3">
+            
+               <h2>PROTOCOLO COVID</h2>
+               <p> En el colegio contamos con un protocolo respecto a las medidas sanitarias siguiendo el lineamiento del Gobierno de Chile en este año 2022 en el cual las clases serán presenciales. </p>
+         </div>
+
+         <div class="caja col-sm-3">
+               
+               <h2>CERTIFICADOS</h2>
+               <p> Contamos con tramites en linea en el sitio web principal del colegio Nuevos Horizontes en el cual puedes solicitar certificados de Alumno Regular para este año 2022 para diversas necesidades. </p>
+         </div>
+      </div>
+      <br> <br>
+      <div class="row d-flex justify-content-center">
+         <div class="caja col-sm-3">
+               
+               <h2>INFRAESTRUCTURA</h2>
+               <p> Somos una institucion preocupada de nuestros estudiantes, por lo cual cumplimos con todas las medidas de seguridad en la infraestructura del establecimiento además de contar con Laboratorios de computación, espacios recreativos amplios, casino, etc. </p>
+         </div>
+
+         <div class="caja col-sm-3">
+               
+               <h2>BIBLIOTECA</h2>
+               <p> En el establecimiento tenemos una Biblioteca para nuestros estudiantes, en la cual existen fechas programadas en las cuales se realizaran actividades interactivas para los diferentes cursos y asignaturas. Además contamos con libros para prestamo de los estudiantes. </p>
+         </div>
+
+         <div class="caja col-sm-3">
+            
+               <h2>NOTICIAS</h2>
+               <p> En nuestro sitio web principal podrás revisar nuestras principales noticias actualizadas día a día contando nuestras actividades o comunicados del establecimiento disponible para todos. </p>
+         </div>
+      </div>
+      <br> <br>
+      
+
+         
+
+         
+     
+   <!--  CUERPO  -->
 
    
   
---
+++ NewHorizons/navbar_1/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css"/>
+    <title>Navbar</title>
+    
+</head>
+
+<body>
+    
+
+
+    <header>
+        <div class="logo-header">
+            <img src="logo-blanco.png">
+        </div>
+        <nav class="nav-menu">
+            <ul>
+                <li><a href="#">INICIO</a></li>
+                <li><a href="#">TEXTO</a></li>
+                <li><a href="#">TEXTO</a></li>
+                <li><a href="#">INGRESAR</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+    
+    <h1> Hola </h1>
+
+    <div class="grid-layout-prin">
+
+        <div class="caja-prin">
+            <img src="https://via.placeholder.com/350x150" class="img"/>
+            <h2>¡Gracias por ser parte de Colegio Nuevos Horizontes!</h2>
+        </div>
+
+    </div>
+
+    <div class ="grid-layout">
+
+        <div class="caja">
+            <img src="https://via.placeholder.com/350x150" class="img"/>
+            <h2>ADMISIÓN 2023</h2>
+            <p> En el colegio Nuevos Horizontes estamos preparados para recibir a nuevos estudiantes en la comunidad educativa. Puedes encontrar más información en el sitio web del colegio para saber como continuar con las matriculas de alumnos ya ingresados. </p>
+        </div>
+
+        <div class="caja">
+            <img src="https://via.placeholder.com/350x150" class="img"/>
+            <h2>PROTOCOLO COVID</h2>
+            <p> En el colegio contamos con un protocolo respecto a las medidas sanitarias siguiendo el lineamiento del Gobierno de Chile en este año 2022 en el cual las clases serán presenciales. </p>
+        </div>
+
+        <div class="caja">
+            <img src="https://via.placeholder.com/350x150" class="img"/>
+            <h2>CERTIFICADOS</h2>
+            <p> Contamos con tramites en linea en el sitio web principal del colegio Nuevos Horizontes en el cual puedes solicitar certificados de Alumno Regular para este año 2022 para diversas necesidades. </p>
+        </div>
+
+        <div class="caja">
+            <img src="https://via.placeholder.com/350x150" class="img"/>
+            <h2>INFRAESTRUCTURA</h2>
+            <p> Somos una institucion preocupada de nuestros estudiantes, por lo cual cumplimos con todas las medidas de seguridad en la infraestructura del establecimiento además de contar con Laboratorios de computación, espacios recreativos amplios, casino, etc. </p>
+        </div>
+
+        <div class="caja">
+            <img src="https://via.placeholder.com/350x150" class="img"/>
+            <h2>BIBLIOTECA</h2>
+            <p> En el establecimiento tenemos una Biblioteca para nuestros estudiantes, en la cual existen fechas programadas en las cuales se realizaran actividades interactivas para los diferentes cursos y asignaturas. Además contamos con libros para prestamo de los estudiantes. </p>
+        </div>
+
+        <div class="caja">
+            <img src="https://via.placeholder.com/350x150" class="img"/>
+            <h2>NOTICIAS</h2>
+            <p> En nuestro sitio web principal podrás revisar nuestras principales noticias actualizadas día a día contando nuestras actividades o comunicados del establecimiento disponible para todos. </p>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
