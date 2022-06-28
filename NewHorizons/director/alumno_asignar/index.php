af3ccf1d10421094be9255a5ea77a41d124ef81e
jorge <jorge.ignacio131198@gmail.com>
2022-06-27 22:07:44 -0400

    variable obtenida desde el select

---
+++ NewHorizons/director/alumno_asignar/index.php
@@ -0,0 +1,292 @@
+<?php 
+include '../seguridad_director.php';    //BD, SEGURIDAD NIVEL, SESSION.
+$datos_cursos = $mysqli->query("SELECT * FROM cursos");
+$curso = '';
+if(isset($_GET['curso'])) {
+$curso = $_GET['curso'];
+}
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Lista de Alumnos </title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"> <!-- BOOSTRAP -->
+
+    <script src="https://code.jquery.com/jquery-3.6.0.js"  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script> <!-- Jquery -->
+    
+   
+    <script>
+        $(document).ready(function() {
+	    $("#id_alumno").change(function() {
+
+		// var selectedVal = $("#myselect option:selected").text();
+		var selectedVal = $("#id_alumno option:selected").val();
+        // alert($('#id_alumno').val());
+
+        });
+     });
+        
+     
+    </script>
+    
+
+    <style>
+        .col1{
+            Height:650px; overflow-y:scroll;
+        }
+
+        .busc{
+            margin-top: 31px;
+        }
+    </style>
+
+</head>
+<body>
+<!-- scripts para boostrap y popper -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>    
+
+
+
+
+
+    <?php 
+    include 'navside.php';
+    ?>
+
+
+
+
+<!-- Inicio Gestor de usuarios--  admin -->   
+<div class="container-fluid mt-5">
+       <div class="row justify-content-center">
+           <div  class="col-md-7">  <!-- Primer col, las siguientes ALERTAS tienen que estar entre medio de aca para que aparezcan dentro del primer col   -->
+
+
+
+               <div  class="card ">
+                   <div class=" card-header">
+                       
+                            <form action="">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="mb-3">
+                                            <label class="form-label lab" for="_6"> Filtrar por curso</label > 
+                                            <select name="curso" class="form-control"  required  id="_6" >
+                                                <option disabled selected value >  </option>
+                                                    <?php
+                                                    $sqlTipo = "SELECT *,
+                                                    cursos.ID as curid,
+                                                    cursos.NOMBRE as curnom,
+                                                    grados.NIVEL as graniv
+                                                    FROM cursos 
+                                                    INNER JOIN grados 
+                                                    ON cursos.ID_GRADO  = grados.ID";
+                                                    $dataNivel = mysqli_query($mysqli, $sqlTipo);
+                                                    //el siguiente codigo: El PRIMER ECHO ID es lo dato que se enviara, en este caso el ID, 
+                                                    //el utf8_encode es el dato de referencia a mostrar, es decir el nombre JUNTO EL NUMERO DEL ID
+                                                    while($data = mysqli_fetch_array($dataNivel)){ ?>
+                                                <option value="<?php echo $data["curid"]; ?>"><?php echo utf8_encode($data['graniv'] . ' ' .$data['curnom'] ); ?>
+        
+                                                    <?php } ?>
+                                            </select>  
+                                        </div>   
+                                        
+                                    </div>
+                                    <div class="col-6">           
+                                        <input type="submit" class="btn btn-primary busc" value="buscar">
+                                    </div>
+                                
+                                </div>
+                            </form>
+                   </div>
+
+
+
+
+
+                    <?php 
+
+
+                        if(empty($curso)){
+                            $query =  "SELECT * FROM alumnos  ";
+                            } else{
+                                
+                            $query = "SELECT * FROM alumnos WHERE ID_CURSO LIKE '{$curso}%'  ";   //OR ID LIKE '{$input}%' OR EMAIL LIKE '{$input}%' OR NIVEL LIKE '{$input}%'
+                            }
+
+
+
+                        $result = mysqli_query($mysqli, $query);
+                        if(mysqli_num_rows($result) > 0 ){?>
+
+
+
+
+                            <div  id="col1" class="p-4 ">
+                                        <table class="table align-middle">
+                                                <thead>
+                                                    <tr>
+                                                        
+                                                        <th scope="col">#</th>
+                                                        <th scope="col">Nombre completo</th>                              
+                                                        <th scope="col">Correo electrónico</th>
+                                                        <th scope="col">Nivel</th>
+
+                                                        <th scope="col" colspan="2">Opciones</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody >
+                                                    <?php     //IMPRIMIR DATOS EN LOS td
+                    
+                                                    while($fila =mysqli_fetch_assoc($result) ) {
+                        
+                                                    ?>
+                                                    <tr >
+
+                                                        <td scope="row"><?php echo $fila['ID']; ?></td>
+                                                        <td ><?php echo $fila['NOMBRE']; ?></td>
+                                                        <td ><?php echo $fila['EMAIL']; ?></td>
+                                                        <td ><?php echo $fila['NIVEL']; ?></td>
+
+                                                        <td><a class="text-primary" href="editar.php?codigo=<?php echo $fila['ID']; ?>">        <i class="bi bi-pencil-square"></i></a>  </td>
+                                                        <td><a onclick="return confirm('¿estas seguro de eliminar a este usuario?')" class="text-danger" href="c_eliminar.php?codigo=<?php echo $fila['ID']; ?>">   <i class="bi bi-trash"></i></a>  </td>  
+                                                        <!-- le envia por la url el id del usuario al c_eliminar -->
+                                                        
+                                                    </tr>
+                                                    <?php
+                                                    }
+                                                    ?>
+                                                
+                                                </tbody>
+                                        </table>
+                                    </div>
+                            <?php 
+
+                        }else{
+                            echo'<h6 class="text-danger text-center mt-3">Datos no encontrados </h6>';
+                        }
+
+                    ?>
+
+               </div>
+               <br>  <br>
+           </div> <!-- TERMINO PRIMER COL  --> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
+
+           <div class="col-md-4">    <!-- INICIO SEGUNDO COL  -->
+               <div class="card">
+                 
+                   <div class="card-header">
+                       Asignar alumno al curso:
+                   </div>
+                   <form action="c_asignar.php" method="POST" class="p-4" >
+
+                        <div class="mb-3">
+                            <label for="id_alumno class="form-label">Alumno: </label>
+                            <select  class="form-control"  required id="id_alumno">
+
+
+                            <!-- Este option es el dato del profesor -->
+                         
+
+                            <option value=""   >
+
+                                        <?php
+                                        $sqlAlumno = "SELECT * FROM matriculados WHERE ASIGNADO LIKE '' order by ID";
+                                        $dataProfe = mysqli_query($mysqli, $sqlAlumno);
+
+                                        
+                                        while($data = mysqli_fetch_array($dataProfe)){ 
+                                        ?>
+
+
+                                        <!-- y este option las opciones -->
+                                        <option  value="<?php echo $data["ID"]; ?>"><?php echo utf8_encode( $data['NOMBRE1_ALUMNO'] . ' '. $data['NOMBRE2_ALUMNO'] . ' '. $data['APELLIDO1_ALUMNO'] 
+                                        . ' '. $data['APELLIDO2_ALUMNO'] ); ?>
+
+    
+                                        <?php } ?>
+
+                                       
+
+                           </select>
+
+                           <div id="mensaje">
+                                <p><?php  echo $id_alumno ?></p>
+                            </div>
+
+                        </div>
+                            
+                        <div class="mb-3">
+                            <label class="form-label lab" for="_6">Curso</label > 
+                            <select name="curso" class="form-control"  required  id="_6" >
+                                <option disabled selected value >  </option>
+                                    <?php
+                                    $sqlTipo = "SELECT *,
+                                    cursos.ID as curid,
+                                    cursos.NOMBRE as curnom,
+                                    grados.NIVEL as graniv
+                                    FROM cursos 
+                                    INNER JOIN grados 
+                                    ON cursos.ID_GRADO  = grados.ID";
+                                    $dataNivel = mysqli_query($mysqli, $sqlTipo);
+                                    //el siguiente codigo: El PRIMER ECHO ID es lo dato que se enviara, en este caso el ID, 
+                                    //el utf8_encode es el dato de referencia a mostrar, es decir el nombre JUNTO EL NUMERO DEL ID
+                                    while($data = mysqli_fetch_array($dataNivel)){ ?>
+                                <option value="<?php echo $data["curid"]; ?>"><?php echo utf8_encode($data['graniv'] . ' ' .$data['curnom'] ); ?>
+
+                                    <?php } ?>
+                            </select>  
+                        </div>   
+                      
+
+                        
+                       
+ 
+
+
+                        
+                        <div class="d-grid mt-5">
+                            <input type="submit" class="btn btn-primary" value="Registrar">
+                        </div>
+
+                   </form>
+
+               </div>
+               <br>
+           </div>
+       </div>
+   </div>
+
+
+
+</body>
+</html>
